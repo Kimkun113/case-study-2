eda8ccb4b588335933dcb2bfbd3d9312e647fb1b
Kimkun113 <98795200+Kimkun113@users.noreply.github.com>
2022-08-08 22:36:05 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+    git
+// sau một khoảmh thời gian thực hiện 1 lần
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let Alive = setInterval(function () {
+  // lấy vị trí y hiện tại của khủng long
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  // lấy vị trí x hiên tại của xương rồng
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    // collision
+    alert("Game Over!");
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
